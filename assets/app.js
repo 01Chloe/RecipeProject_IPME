d042809825037a09d6bd32591d26e691e2e3ab8a
01Chloe <allier.chloe@gmaim.com>
2025-07-22 12:04:05 +0200

    style organization

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,9 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/header.css';
+import './styles/footer.css';
+import './styles/fonts.css';
+import './styles/variables.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
