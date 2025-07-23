8d7d1ef06e0c05b93fdea76fb073cfa90157943a
01Chloe <allier.chloe@gmaim.com>
2025-07-23 09:27:28 +0200

    add pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\RecipeRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(RecipeRepository $recipeRepository): Response
+    public function index(RecipeRepository $recipeRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $recipesAndCategories = $recipeRepository->findAll();
+        $recipesAndCategories = $paginator->paginate(
+            $recipeRepository->findBy(['status' => 300]),
+            $request->query->getInt('page', 1),
+            10
+        );
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
