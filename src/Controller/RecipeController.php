50d596dca696f5cc861f859ee1d4bc995a222c2c
01Chloe <allier.chloe@gmaim.com>
2025-07-23 09:45:07 +0200

    add recipe link

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RecipeController extends AbstractController
+{
+    #[Route('/recipe/{id}', name: 'app_recipe')]
+    public function index(string $id, Request $request, RecipeRepository $recipeRepository): Response
+    {
+        $recipe = $recipeRepository->findOneBy(["id" => $id]);
+        dump($recipe);
+
+        return $this->render('recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+}
